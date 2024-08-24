f59dbc51ccbe5dce3a4206b2d199371b4c7cf9f5
Jeffrey <wsadtc111@qq.com>
2024-08-24 12:06:56 +0800

    fix the bug of debugExercise1 round method

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -5,8 +5,8 @@ package DebugExercise;
  * feature work.
  */
 public class DebugExercise1 {
-    public static int divideThenRound(int top, int bottom) {
-        float quotient = (float)top / bottom;
+    public static int divideThenRound(float top, float bottom) {
+        float quotient = top / bottom;
         int result = Math.round(quotient);
         return result;
     }
